044b2f65c9ec81950a05eb2a83d91ffb33b3091b
Andre Regino <reginopanai@gmail.com>
2024-10-19 13:56:42 -0600

    Created "about" and "review" routes

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,15 +1,23 @@
-// const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function(req, res, next) {
     try {
-      const nav = `<p>Hola</p>`;
-      res.render("index", { title: "Home", nav });
+      res.render("index", { title: "Search for Courses"});
     } catch (error) {
-      console.error("Error fetching nav:", error);
-      next(new Error("Error fetching navigation"));
+      console.error("Error:", error);
+      next(new Error("Error Rendering"));
     }
   };
   
+baseController.buildDetails = async function(req, res, next) {
+  try{
+    res.render("about", { title: "About *Insert CourseID here*"});
+  } catch (error) {
+    console.error("Error:", error);
+    next(new Error("Error Rendering"));
+  }
+
+}
+
 
 module.exports = baseController;
