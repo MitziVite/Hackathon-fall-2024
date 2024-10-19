69937ee0c6bae45c18f4de985bfb6e2ef8366e61
kevingonmed <114196683+kevingonmed@users.noreply.github.com>
2024-10-19 01:02:29 -0600

    Create endpoint working

---
+++ database.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // Load environment variables from .env file
+const uri = process.env.MONGOKEY;
+const client = new MongoClient(uri);
+
+const connectDB = async () => {
+    try {
+        await client.connect();
+        console.log('MongoDB connected successfully');
+        return client.db(); // Return the database object
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit the process with failure
+    }
+};
+
+module.exports = { connectDB };
