a694e50849238fb520d6672b59dbf993000f9918
Andre Regino <reginopanai@gmail.com>
2024-12-05 19:57:39 -0700

    Added enter key event listener, and created utility method to insert Course Learning Outcomes

--- utilities/index.js
+++ utilities/index.js
@@ -14,4 +14,17 @@ Util.getNav = async function (req, res, next) {
 // Manejo de errores
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
+
+
+Util.createOutcomes = function(outcomesList){
+
+    console.log('Starting to create HTML')
+    let outcomeHTML = "<ul>";
+    outcomesList.forEach(outcome => {
+        outcomeHTML += `<li>${outcome['value']}</li>`;
+    });
+    outcomeHTML += `</ul>`
+    console.log('creted HTML list items')
+    return outcomeHTML;
+}
 module.exports = Util;
