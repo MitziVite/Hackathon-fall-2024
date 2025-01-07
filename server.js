687cfb6895e0ad6054428a0aa1f019c3ab9288af
Andre Regino <reginopanai@gmail.com>
2025-01-06 23:27:36 -0700

    started implementing code for user session and flash messages

--- server.js
+++ server.js
@@ -7,6 +7,57 @@ const app = express()
 const routes = require("./routes/routes")
 const utilities = require("./utilities");
 const bodyParser = require("body-parser");
+const session = require("express-session")
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
+const { connectDB } = require('./database.js')
+// const { MongoClient } = require('mongodb');
+// const uri = process.env.MONGOKEY;
+// const client = new MongoClient(uri);
+
+var db = null;
+
+connectDB()
+.then(result => {
+  db = result; // "Hello, World!"
+  // console.log(db)
+})
+.catch(error => {
+  console.error(error);
+});
+
+// console.log(db)
+
+
+// Set up express-session with MongoDB store
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false, 
+//     saveUninitialized: false,
+//     store: MongoStore.create({
+//       client: client, // Use the existing MongoDB client
+//       dbName: db.databaseName, // Use the connected database name
+//       collectionName: 'sessions', 
+//       ttl: 14 * 24 * 60 * 60, // 14 
+//     }),
+//     cookie: {
+//       secure: false, 
+//       httpOnly: true, 
+//       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+//     },
+//   })
+// );
+
+// Express Messages Middleware
+app.use(flash());
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+
 
 /* ***********************
  * Routes
