13679ee44ce548cdc1d8a331031e6fe7f33a2327
Andre Regino <reginopanai@gmail.com>
2025-01-09 22:19:40 -0700

    connected flash messages and put a placeholder for favicon

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ connectDB()
 
 
 // Express Messages Middleware
-// app.use(require('connect-flash')())
+app.use(require('connect-flash')())
 // app.use(function(req, res, next){
-//   res.locals.messages = require('express-messages')(req, res)
-//   next()
+  // res.locals.messages = require('express-messages')(req, res)
+  // next()
 // })
 
 
