dea4c3e88977204dffc53c75b63a5903d2cdfffb
Andre Regino <reginopanai@gmail.com>
2025-01-10 13:58:46 -0700

    created a messages for errors

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,25 +2,31 @@ const Review = require('../models/reviewModel.js');
 
 
 const createReview = async function (req, res, next) {
-    let post = req.body
-    let form = {
-        codeClass: post.codeClass,
-        semester: post.semester, 
-        year: post.year,
-        grades: post.grades,
-        difficulty: post.difficulty,
-        hoursPerWeek: post.hoursPerWeek,
-        evaluationType: post.evaluationType,
-        overallSatisfaction: post.overallSatisfaction,
-        onlinevsperson: post.classType,
-        teacher: post.teacher,
-        comment: post.comment,
-        db: post.db
+    try{
+        let post = req.body
+        let form = {
+            codeClass: post.codeClass,
+            semester: post.semester, 
+            year: post.year,
+            grades: post.grades,
+            difficulty: post.difficulty,
+            hoursPerWeek: post.hoursPerWeek,
+            evaluationType: post.evaluationType,
+            overallSatisfaction: post.overallSatisfaction,
+            onlinevsperson: post.classType,
+            teacher: post.teacher,
+            comment: post.comment,
+            db: post.db
+        }
+        let review = new Review(form);
+        review.createReview();
+        req.flash('message', 'Your feedback was saved successfully')
+        res.redirect("/");
     }
-    let review = new Review(form);
-    review.createReview();
-    req.flash('message', 'Your feedback was saved successfully')
-    res.redirect("/");
+    catch (error) {
+        req.flash('notice', 'There was an issue with your submission')
+        res.status(501).redirect("/forms/rateClassForm")
+    }
 };
 
 const getReview = async function (req, res, next) {
